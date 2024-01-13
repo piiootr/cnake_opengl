f9e90c215465a3aa776187d82c8490411b139fb3
piiootr <piiotrcontact@gmail.com>
2024-01-13 01:11:01 +0100

    hello rectangle

--- Makefile
+++ Makefile
@@ -1,6 +1,39 @@
+CC		:= clang
+CFLAGS	:= -std=c99 -Os
+CFLAGS	+= -Wextra -Wall -Werror
+CFLAGS	+= -Wfloat-equal -Winit-self
+CFLAGS	+= -Wundef -Wshadow -Wpointer-arith
+CFLAGS	+= -Wcast-align -Wstrict-prototypes
+#CFLAGS	+= -Wstrict-overlfow=5 -Waggregate-return
+CFLAGS	+= -Wwrite-strings -Wcast-qual
+CFLAGS	+= -Wswitch-default -Wswitch-enum
+CFLAGS	+= -Wconversion -Wunreachable-code
+
+CFLAGS	+= -Ilib/glad/include -Ilib/glfw/include
+LDFLAGS	:= lib/glad/src/glad.o lib/glfw/src/libglfw3.a
+
+LDFLAGS	+= -framework OpengL -framework IOKit -framework CoreVideo -framework Cocoa
+
+GLAD := lib/glad/src/glad.o
 GLFW := lib/glfw/src/libglfw3.a
 
-glfw:
-	cd lib/glfw && \\
-	cmake . -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF && \\
+.PHONY: all main
+
+all: libs main
+
+libs: $(GLAD) $(GLFW)
+
+$(GLFW): $(wildcard lib/glfw/src/*.c lib/glfw/src/*.m)
+	@echo compiling glfw
+	@cd lib/glfw && \
+	cmake . -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET="11.0" && \
 	make
+
+$(GLAD): lib/glad/src/gl.c
+	@echo compiling glad
+	@cd lib/glad && \
+	$(CC) -o src/glad.o -Iinclude -c src/gl.c
+
+main: src/main.c
+	@echo compiling main
+	@$(CC) -o main $(CFLAGS) $(LDFLAGS) src/main.c
--- src/main.c
+++ src/main.c
@@ -2,10 +2,33 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
-int main(){
-    printf("hello, world\n");
+const char *shaderv_src =
+    "#version 330 core\n"
+    "layout (location = 0) in vec2 vert_pos;"
+    "void main() {"
+    "   gl_Position = vec4(vert_pos, 0.0, 1.0);"
+    "}\0";
+const char *shaderf_src =
+    "#version 330 core\n"
+    "out vec4 frag_col;"
+    "void main() {"
+    "   frag_col = vec4(vec3(0.9451), 1.0);"
+    "}\0";
 
+int main(){
     GLFWwindow *window;
+    GLuint      shaderv, shaderf, shaderp;
+    GLuint      VAO, VBO, EBO;
+    GLuint      indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    GLfloat     vertices[] = {
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0
+    };
 
     if (!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -18,13 +41,47 @@ int main(){
     glfwMakeContextCurrent(window);
     if (!gladLoadGL(glfwGetProcAddress)) return -1;
 
+    shaderv = glCreateShader(GL_VERTEX_SHADER);
+    shaderf = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(shaderv, 1, &shaderv_src, NULL);
+    glShaderSource(shaderf, 1, &shaderf_src, NULL);
+    glCompileShader(shaderv);
+    glCompileShader(shaderf);
+
+    shaderp = glCreateProgram();
+    glAttachShader(shaderp, shaderv);
+    glAttachShader(shaderp, shaderf);
+    glLinkProgram(shaderp);
+
+    glDeleteShader(shaderv);
+    glDeleteShader(shaderf);
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (void *) 0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.375, 0.375, 1.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderp);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (const void *) 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    glDeleteProgram(shaderp);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
